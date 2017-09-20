492e9f993b111c1233317b3bacd4925b04e3c0e2
Zeeshan Hanif <zeeshi13@gmail.com>
2017-09-20 16:12:47 -0400

    Project 1

---
+++ Assigment 1/Lab1/Assets/Scripts/SliderScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderScript : MonoBehaviour {
+
+	public PlayerController ball;
+
+	void onValueChanged(int newValue){
+		
+		ball.speed = newValue;
+	}
+}
+
+	
